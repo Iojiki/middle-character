f18cb363a9406f4150543e9e1d1fc814ca298b82
Troy <eeohjeeki@gmail.com>
2018-03-23 02:31:25 -0500

    updated file

--- middlechar/Program.cs
+++ middlechar/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace middlechar
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Please enter a line of text");
+        string testWord = Console.ReadLine();
+        if (testWord.Length == 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(String.Empty);
+            //So program doesn't immediatly end
+            Console.ReadLine();
         }
+        else if ((testWord.Length % 2) == 0)
+        {
+            int middleOne = ((testWord.Length) / 2) - 1;
+            int middleTwo = ((testWord.Length) / 2);
+
+            string evenPair = (testWord[middleOne]).ToString() + (testWord[middleTwo]).ToString();
+            Console.WriteLine(evenPair);
+            //So program doesn't immediatly end
+            Console.ReadLine();
+
+        }
+        else
+        {
+            int middleChar = (testWord.Length - 1) / 2;
+            char letter = testWord[middleChar];
+
+            Console.WriteLine(letter);
+            //So program doesn't immediatly end
+            Console.ReadLine();
+        }
+
     }
 }
